667edf21ee0e561f9e558656c4f4149440fcc2a4
TatianeKishi <102524752+TatianeKishi@users.noreply.github.com>
2023-03-15 11:19:15 -0300

    15-03-2023

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
