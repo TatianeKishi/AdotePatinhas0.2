ec45f28896185adc3ff63f0e16e16872b2f7511d
TatianeKishi <102524752+TatianeKishi@users.noreply.github.com>
2023-03-29 11:10:31 -0300

    29-3-2023

--- src/pages/Interests/index.js
+++ src/pages/Interests/index.js
@@ -1,13 +1,25 @@
 import Navbar from '../../components/NavBar/index'
 import Footer from '../../components/Footer/footer'
 import './Interests.css';
+import notInterests from '../../img/notInterests.svg'
+import ButtonRemove from '../../components/Buttons/Button'
 // import Interests from './'
 function Interests () {
     return (
         <div className="interests">
             <Navbar/>
-                <div>
-                    <img src=''>
+                <div className='interests_section'>
+                    <div className='interests_section-container'>
+                        <div className='interests_section-image'>
+                            <img src={notInterests}/>
+                        </div>
+                        <div className='interests_section-button'>
+                           <ButtonRemove corFundo='#5A3938'>ESCOLHA UM ANIMAL</ButtonRemove>
+                        </div>
+                    </div>
+                    <div className='interests-section-cards'>
+
+                    </div>
                 </div>
             <Footer/>
         </div>
